acdad75d5b0f7e47bdb8348a950185a7290a6e69
evgeniyDorogoy <evgeniy.dorogoy@gmail.com>
2018-02-19 23:09:23 +0200

    Add some DataProvider unittests (WIP). Pytest set as Dev requirement.

--- tests/test_dropbox_dataprovider.py
+++ tests/test_dropbox_dataprovider.py
@@ -1,6 +1,49 @@
+from collections import namedtuple
+from unittest.mock import patch, Mock, PropertyMock
+import pytest
+from transport.data_provider import DropBoxDataProvider
 
 
-class TestDropboxDataprovider:
+class TestDropboxDataProvider:
 
-    def test_smoke(self):
-        assert True
+    @patch('transport.data_provider.DataProviderBase.make_get_request',
+           new=Mock(return_value=Mock(status_code=200, text='Ok')))
+    def test_smoke_positive(self):
+        result = DropBoxDataProvider().smoke()
+        assert result[0] == 200
+        assert result[1] == 'Ok'
+
+    def test_smoke_missed_url(self):
+        with pytest.raises(ValueError):
+            result = DropBoxDataProvider()
+            result.smoke_url = None
+            result.smoke()
+
+    def test_api_smoke_positive(self):
+        result = DropBoxDataProvider()
+        result.dbx = Mock()
+        result.api_smoke()
+
+    def test_api_smoke_negative(self):
+        with pytest.raises(Exception):
+            result = DropBoxDataProvider()
+            result.dbx.files_list_folder = Mock(return_value=Mock(entries=None))
+            result.api_smoke()
+
+    def test_get_list_of_object(self):
+        result = DropBoxDataProvider()
+
+        dummy_file_object = Mock(path_lower='/monefysource/monefy.data.1-11-11.csv')
+
+        # Following code are necessary for override 'name' attribute of 'Mock' object
+        n = PropertyMock(return_value='Monefy.Data.1-11-11')
+        type(dummy_file_object).name = n
+
+        result.dbx.files_list_folder = Mock(return_value=Mock(entries=tuple([dummy_file_object])))
+
+        list_of_objects = result.get_list_of_objects()
+
+        assert isinstance(list_of_objects, list)
+        assert isinstance(list_of_objects[0], tuple)
+        assert list_of_objects[0].filename == 'Monefy.Data.1-11-11'
+        assert list_of_objects[0].filepatch == '/monefysource/monefy.data.1-11-11.csv'
--- transport/data_provider.py
+++ transport/data_provider.py
@@ -12,12 +12,17 @@ class DataProviderBase:
 
     smoke_url = None
 
-    def smoke(self) -> None:
+    @staticmethod
+    def make_get_request(url) -> requests.models.Response:
+        r = requests.get(url)
+        r.raise_for_status()
+        return r
+
+    def smoke(self) -> tuple:
         if not self.smoke_url:
             raise ValueError('You have to specify URL for smoke testing')
-
-        r = requests.get(self.smoke_url)
-        r.raise_for_status()
+        result = self.make_get_request(self.smoke_url)
+        return result.status_code, result.text
 
     def api_smoke(self):
         raise NotImplementedError
@@ -34,11 +39,10 @@ class DropBoxDataProvider(DataProviderBase):
         if not self.dbx.files_list_folder('').entries:
             raise Exception('There are no folders in your Dropbox account')
 
-    def get_list_of_objects(self, path='', recursive=False) -> namedtuple:
-        result = namedtuple('Result', ['filename', 'filepath'])
-
+    def get_list_of_objects(self, path='', recursive=False) -> list:
+        result = namedtuple('Result', ['filename', 'filepatch'])
         return [result(el.name, el.path_lower) for el in self.dbx.files_list_folder(path=path, recursive=recursive).entries]
 
     def get_files(self) -> None:
         for file in self.get_list_of_objects(path=dpc.source_folder):
-            self.dbx.files_download_to_file(os.path.join(dpc.destination_folder, file.filename), file.filepath)
+            self.dbx.files_download_to_file(os.path.join(dpc.destination_folder, file.filename), file.filepatch)
