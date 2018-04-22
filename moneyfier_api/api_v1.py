84d981ebf210972f6f6c69a14c6c966f9a4796c7
evgeniyDorogoy <evgeniy.dorogoy@gmail.com>
2018-04-22 12:53:29 +0300

    Some refactoring etc. Changes to be committed: 	new file:   Dockerfile 	modified:   Pipfile 	modified:   Pipfile.lock 	modified:   moneyfier_api/api_v1.py 	copied:     moneyfier_api/transport_resource.py -> moneyfier_api/data_resource.py 	modified:   moneyfier_api/transport_resource.py

--- moneyfier_api/api_v1.py
+++ moneyfier_api/api_v1.py
@@ -1,6 +1,7 @@
 from moneyfier_api.app import app
+from moneyfier_api.data_resource import GetSumByCategories, UpdateDatabaseWithLastData, GetAllRecords
 from moneyfier_api.service_resource import CreateTables, DropTAbles
-from moneyfier_api.transport_resource import DropboxResources, UpdateDatabaseWithLastData, GetAllRecords
+from moneyfier_api.transport_resource import DropboxResources
 
 # service resources
 app.add_route(CreateTables.as_view(), '/create-tables')
@@ -12,3 +13,4 @@ app.add_route(UpdateDatabaseWithLastData.as_view(), '/update-with-last-data')
 
 # data manipulation resources
 app.add_route(GetAllRecords.as_view(), '/get-all-records')
+app.add_route(GetSumByCategories.as_view(), '/get-sum-by-categories')
